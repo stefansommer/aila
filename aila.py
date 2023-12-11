d4abab6bb83bcc190de29b4d26add71102966910
Stefan Sommer <sommer@di.ku.dk>
2023-12-11 15:40:03 +0100

    Update aila.py

--- aila.py
+++ aila.py
@@ -13,18 +13,6 @@ Email: sommer@di.ku.dk
 The interface to Aula is written by Morten Helmstedt. E-mail: helmstedt@gmail.com
 see https://helmstedt.dk/2021/05/aulas-api-en-opdatering/
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
 import aula
